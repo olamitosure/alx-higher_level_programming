11b27bfbe3bd997ba63def0856ca24b9fd63897d
olamitosure <olorunsomoolami01@gmail.com>
2023-03-13 07:43:19 -0700

    0x03-python-data_structures update

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-
-
 def new_in_list(my_list, idx, element):
+    new_list = my_list[:]
+    if 0 <= idx < len(new_list):
+        new_list[idx] = element
+        return (new_list)
+    return (my_list)
 
-    if (idx < 0) or (idx > (len(my_list)-1)):
-        return my_list
