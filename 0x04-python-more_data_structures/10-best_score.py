6e35c0167cd8a5993541a8f0ac580e23ab0d98cb
olamitosure <olorunsomoolami01@gmail.com>
2023-03-15 11:37:41 -0700

    0x04-python-more_data_structures update

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    name = ""
+    score = 0
     if not a_dictionary:
         return None
     else:
-        return (max(a_dictionary, key=a_dictionary.get))
-
+        for key in a_dictionary:
+            if a_dictionary[key] > score:
+                score = a_dictionary[key]
+                name = key
+    return name
