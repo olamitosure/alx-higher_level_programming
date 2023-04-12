b830cf2ef5ebeebbb524b40abca4aa64c31d3eae
olamitosure <olorunsomoolami01@gmail.com>
2023-04-12 08:30:33 -0700

    0x0B-python-input_output update

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,50 +1,17 @@
 #!/usr/bin/python3
-"""Reads from standard input and computes metrics
-"""
+""" Function """
+import sys
 
 
-def print_stats(size, status_codes):
-    """Print accumulated metrics
-    """
-    print("File size: {}".format(size))
-    for key in sorted(status_codes):
-        print("{}: {}".format(key, status_codes[key]))
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
-if __name__ == "__main__":
-    import sys
+try:
+    js_list = load_from_json_file("add_item.json")
+except Exception:
+    js_list = []
 
-    size = 0
-    status_codes = {}
-    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-    count = 0
-
-    try:
-        for line in sys.stdin:
-            if count == 10:
-                print_stats(size, status_codes)
-                count = 1
-            else:
-                count += 1
-
-            line = line.split()
-
-            try:
-                size += int(line[-1])
-            except (IndexError, ValueError):
-                pass
-
-            try:
-                if line[-2] in valid_codes:
-                    if status_codes.get(line[-2], -1) == -1:
-                        status_codes[line[-2]] = 1
-                    else:
-                        status_codes[line[-2]] += 1
-            except IndexError:
-                pass
-
-        print_stats(size, status_codes)
-
-    except KeyboardInterrupt:
-        print_stats(size, status_codes)
-        raise
+for arg in sys.argv[1:]:
+    js_list.append(arg)
+save_to_json_file(js_list, "add_item.json")
